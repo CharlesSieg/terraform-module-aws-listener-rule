6b9111c8a52708d518282fb597a3a170beb4855f
Charles Sieg <charlessieg@colibrigroup.com>
2021-10-19 12:06:53 -0500

    Fixes.

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ resource "aws_lb_listener_rule" "rule" {
     dynamic "host_header" {
       for_each = { for i in local.conditions : i.field => i if i.field == "host-header" }
       content {
-        values = [host_header.value.value]
+        values = host_header.value["values"]
       }
     }
     dynamic "path_pattern" {
       for_each = { for i in local.conditions : i.field => i if i.field == "path-pattern" }
       content {
-        values = [path_pattern.value.value]
+        values = path_pattern.value["values"]
       }
     }
   }
